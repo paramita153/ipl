df2a066bd6eec79b3bfad6df5662e11d81db30b7
paramita153 <paramita153@gmail.com>
2021-04-28 16:05:59 +0530

    cimmit csv data

---
+++ src/main/java/com/code/ipl/data/MatchInput.java
@@ -0,0 +1,23 @@
+package com.code.ipl.data;
+
+public class MatchInput {
+  
+    
+ private String id;
+ private String city; 
+ private String date;
+ private String player_of_match; 
+ private String venue;
+ private String neutral_venue; 
+ private String team1; 
+ private String team2;
+ private String toss_winner; 
+ private String toss_decision; 
+ private String winne;
+ private String result; 
+ private String result_margin; 
+ private String eliminator; 
+ private String method; 
+ private String umpire1; 
+ private String umpire2;
+}
